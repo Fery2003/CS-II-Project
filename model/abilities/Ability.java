2988ed680cf95f593fc17f82cd3b8bcc90696071
Fery2003 <feras.nizar2003@gmail.com>
2022-03-19 19:41:36 +0200

    packages initialised, "abilities" package 98% done

---
+++ model/abilities/Ability.java
@@ -0,0 +1,65 @@
+package model.abilities;
+
+public class Ability {
+
+    private String name;
+    private int manaCost;
+    private int baseCooldown;
+    private int currentCooldown; // READ AND WRITE
+    private int castRange;
+    private int requiredActionPoints;
+    private AreaOfEffect castArea;
+
+    public Ability(String name, int cost, int baseCooldown, int castRange, AreaOfEffect area, int required) {
+
+        this.name = name;
+        this.manaCost = cost;
+        this.baseCooldown = baseCooldown;
+        this.castRange = castRange;
+        this.castArea = area;
+        this.requiredActionPoints = required;
+
+    }
+
+    public Ability() { // Empty constructor just in case
+        this.name = "";
+        this.manaCost = 0;
+        this.baseCooldown = 0;
+        this.castRange = 0;
+        // this.castArea = ???;
+        this.requiredActionPoints = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCost() {
+        return this.manaCost;
+    }
+
+    public int getBaseCooldown() {
+        return this.baseCooldown;
+    }
+
+    public int getCurrentCooldown() {
+        return this.currentCooldown;
+    }
+
+    public void setCurrentCooldown(int c) { // since it's READ & WRITE not READ ONLY
+        this.currentCooldown = c;
+    }
+
+    public int getCastRange() {
+        return this.castRange;
+    }
+
+    public int getRequiredActionPoints() {
+        return this.requiredActionPoints;
+    }
+
+    public AreaOfEffect getCastArea() {
+        return this.castArea;
+    }
+
+}
---
+++ model/abilities/HealingAbility.java
@@ -0,0 +1,21 @@
+package model.abilities;
+
+public class HealingAbility extends Ability {
+
+    private int healAmount; // READ AND WRITE
+
+    public HealingAbility(String name, int cost, int baseCooldown, int castRange, AreaOfEffect area, int required,
+            int healAmount) {
+        super(name, cost, baseCooldown, castRange, area, required);
+        this.healAmount = healAmount;
+    }
+
+    public int getHealAmount() {
+        return this.healAmount;
+    }
+
+    public void setHealAmount(int h) { // since it's READ & WRITE not READ ONLY
+        this.healAmount = h;
+    }
+
+}
